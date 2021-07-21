f23f9c594ad46f24a14e84cdebd57f0081e21258
Ben Bauer <ben.bauer1@gmail.com>
2021-07-21 13:47:47 -0500

    creates password, stores password locally, checks inputs

--- main.py
+++ main.py
@@ -1,12 +1,46 @@
 import tkinter
+from tkinter import messagebox
 import random
+from tkinter.constants import END
+from typing import Text
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    pass
+    input_password.delete(0,END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = ""
+    for char in password_list:
+        password += char
+
+    input_password.insert(0,password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def store_password():
-    pass
+    if(input_password.get() == "" or input_user.get() == "" or input_website.get() == ""):
+        messagebox.showinfo(title="Error!", message="Please complete all inputs")
+
+    with open("passwords.txt", "a") as file:
+        file.write(f"{input_website.get()} | {input_user.get()} | {input_password.get()}")
+        input_website.delete(0, END)
+        input_password.delete(0,END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = tkinter.Tk()
